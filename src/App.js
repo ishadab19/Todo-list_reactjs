801845e21b6c6bb44fe38107064276e51cf68161
shadab <me.shadab19@gmail.com>
2023-05-12 00:40:41 +0530

    [Added-To-do-List reactapp]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [inputVal, SetInputVal] = useState("");
+  const [inputArr, SetInputArr] = useState([]);
+
+  //add items in listfunction
+  const addItems = () => {
+    if (!inputVal) {
+      alert("please enter some data");
+    } else {
+      SetInputArr([...inputArr, inputVal]);
+      SetInputVal("")
+    }
+  }
+  //delete items in listfunction
+  const DeleteItem = (id) => {
+    const newArr = inputArr.filter((val, ind) => {
+      return id !== ind
+    });
+    SetInputArr(newArr)
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <div>
+        Todo-List <br />
+        <input type='text' value={inputVal} onChange={(e) => { SetInputVal(e.target.value) }}
+        />
+        <button onClick={addItems}>AddItems</button>
+      </div>
+      <div>
+        <ul>
+          {
+            inputArr.map((val, ind) => {
+              return <li key={ind}>{val} <button onClick={() => DeleteItem(ind)}>delete</button></li>
+
+            })
+          }
+        </ul>
+
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
